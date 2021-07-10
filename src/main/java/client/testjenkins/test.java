66f87a49e821d58068b0dc052ab11d8edd626eb0
Mathieu Beaudoin <0432442@.bdeb.qc.ca>
2021-07-10 18:38:01 -0400

    initial

---
+++ src/main/java/client/testjenkins/test.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client.testjenkins;
+
+/**
+ *
+ * @author leils
+ */
+public class test {
+    private String user;
+
+    public test() {
+    }
+
+    public test(String user) {
+        this.user = user;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+    
+}
